a6716591c5bba0715831a236a3884d311c991816
DocentesBrown <docentxsbrown@gmail.com>
2026-03-21 21:03:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PDB_CONFIG = {
   APP_NAME: "Planificador Docentes Brown",
-  FUNCTION_URL: "https://TU-PROJECT-REF.supabase.co/functions/v1/generate-plan",
+  FUNCTION_URL: "https://xvoixwbfdxhorsceoygx.supabase.co/functions/v1/generate-plan",
   REQUEST_TIMEOUT_MS: 90000,
   PDF_FILENAME_PREFIX: "planificacion-docentes-brown"
 };
